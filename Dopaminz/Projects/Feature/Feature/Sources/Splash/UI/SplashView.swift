57f823db76e364a598cf7d3456af72a325393b04
Wallaby <avocado34.131@gmail.com>
2024-04-06 21:51:22 +0900

    [Feat] Update splash view animation

--- Dopaminz/Projects/Feature/Feature/Sources/Splash/UI/SplashView.swift
+++ Dopaminz/Projects/Feature/Feature/Sources/Splash/UI/SplashView.swift
@@ -15,22 +15,28 @@ public struct SplashView: View {
   // MARK: - Properties
   
   @State private var ddang1Offset: CGFloat = -500
+  @State private var ddang2Offset: CGFloat = -500
+  @State private var ddang3Offset: CGFloat = -500
   
   
   // MARK: - Views
   
   public var body: some View {
     VStack(spacing: 16) {
-      Image(asset: .logo)
+      Image(uiImage: UIImage(named: "logo")!)
         .resizable()
         .aspectRatio(contentMode: .fit)
         .frame(height: 40)
       
       HStack(spacing: 4) {
-        Image(asset: .ddang1)
+        Image(uiImage: UIImage(named: "ddang1")!)
           .offset(y: ddang1Offset)
-        Image(asset: .ddang2)
-        Image(asset: .ddang3)
+        
+        Image(uiImage: UIImage(named: "ddang2")!)
+          .offset(y: ddang2Offset)
+        
+        Image(uiImage: UIImage(named: "ddang3")!)
+          .offset(y: ddang3Offset)
       }
     }
     .background(Color.white.ignoresSafeArea())
@@ -38,6 +44,19 @@ public struct SplashView: View {
       withAnimation(.spring(duration: 0.3, bounce: 0.2)) {
         ddang1Offset = 0
       }
+      
+      DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+        withAnimation(.spring(duration: 0.3, bounce: 0.2)) {
+          ddang2Offset = 1
+        }
+      }
+      
+      DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+        withAnimation(.spring(duration: 0.3, bounce: 0.2)) {
+          ddang3Offset = 2
+        }
+      }
+      
     }
   }
 }
