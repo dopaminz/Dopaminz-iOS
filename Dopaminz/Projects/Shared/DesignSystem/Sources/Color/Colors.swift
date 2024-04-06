7510e0f8ab32ebaa216e234ee1fe0c0d2fc6a64e
Wallaby <avocado34.131@gmail.com>
2024-04-06 22:09:52 +0900

    [Feat] Add color styles

--- Dopaminz/Projects/Shared/DesignSystem/Sources/Color/Colors.swift
+++ Dopaminz/Projects/Shared/DesignSystem/Sources/Color/Colors.swift
@@ -27,5 +27,41 @@ public extension Color {
     static let logoutBorder = Color(hex: "#EBE3E3")
     static let logoutText = Color(hex: "#8C9097")
     
+    
+    // MARK: - Color systems
+    
+    static let gray800 = Color("#262626")
+    static let gray700 = Color("#434343")
+    static let gray600 = Color("#555555")
+    static let gray500 = Color("#7B7B7B")
+    static let gray400 = Color("#9D9D9D")
+    static let gray300 = Color("#C4C4C4")
+    static let gray200 = Color("#D9D9D9")
+    static let gray100 = Color("#E9E9E9")
+    static let gray50 = Color("#F5F5F5")
+    static let gray10 = Color("#F7F7F7")
+    
+    static let blue900 = Color("#2224CB")
+    static let blue800 = Color("#2C32D6")
+    static let blue600 = Color("#363EE3")
+    static let blue500 = Color("#3B47EF")
+    static let blue400 = Color("#5C66F4")
+    static let blue300 = Color("#7B84F7")
+    static let blue200 = Color("#A3A7F9")
+    static let blue100 = Color("#C9CAFB")
+    static let blue50 = Color("#EAEAFE")
+    
+    static let success500 = Color("#25F152")
+    static let succese400 = Color("#51F668")
+    static let success300 = Color("#7FFA87")
+    static let success200 = Color("#AAFCAD")
+    static let success100 = Color("#CEFECE")
+    static let success50 = Color("#EBFFEB")
+    
+    static let danger500 = Color("#E01507")
+    static let danger400 = Color("#E02B1D")
+    static let danger300 = Color("#FF5A4F")
+    static let danger200 = Color("#F0857D")
+    static let danger100 = Color("#FFF1F0")
 }
 
